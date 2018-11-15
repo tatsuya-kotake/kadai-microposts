82750f5820b24b6f02783335d6d9babe8660dc10
kazuya <k.tatsuya831@gmail.com>
2018-11-15 07:28:46 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+   protect_from_forgery with: :exception
+   
+   include SessionsHelper
+
+  private
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+  
+  def counts(user)
+    @count_microposts = user.microposts.count
+    @microposts = @user.microposts.order('created_at DESC').page(params[:page])
+    counts(@user)
+  end
 end
